d19841b66545abce295d2f46e966a2401d067739
MauiSabily <mauisabily@sabily.info>
2018-09-03 02:59:32 +0800

    addnode
    Test automed addnode

--- auto.sh
+++ auto.sh
@@ -1,3 +1,4 @@
+echo 'addnode=119.81.54.205' > $HOME/.syiling/syiling.conf &&
 sudo apt update &&
 sudo apt-get install build-essential libdb-dev libdb++-dev libboost-all-dev git libssl1.0-dev libboost-all-dev libminiupnpc-dev libevent-dev libcrypto++-dev libgmp3-dev &&
 cd src &&
