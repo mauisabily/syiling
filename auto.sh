b6da07f6ff01354ba05011a9a9cf940eb5c74bf9
MauiSabily <mauisabily@sabily.info>
2018-09-03 00:30:38 +0800

    test auto.sh
    New Script for Ubuntu 18.04.1 LTS
    just run bash auto.sh

---
+++ auto.sh
@@ -0,0 +1,7 @@
+sudo apt update &&
+sudo apt-get install build-essential libdb-dev libdb++-dev libboost-all-dev git libssl1.0-dev libboost-all-dev libminiupnpc-dev libevent-dev libcrypto++-dev libgmp3-dev &&
+cd src &&
+sudo chmod +x leveldb/build_detect_platform &&
+make -f makefile.unix &&
+sudo chmod +x syilingd &&
+sudo cp syilingd /usr/local/bin/
